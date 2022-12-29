a289b54f95d8a37afe7ca89fbc032408051d271c
Mad Max <samumm@mail.ru>
2022-12-29 17:31:19 +0300

    Java Syntax Zero, Level 01, Lecture 02, Task 01

--- Java Syntax Zero/Level 01/Lecture 02/Task 01/Solution.java
+++ Java Syntax Zero/Level 01/Lecture 02/Task 01/Solution.java
@@ -13,11 +13,15 @@
  • Выводимый текст должен соответствовать заданию.
  */
 
-package com.javarush.task.task01.task0142;
+package com.javarush.task.task01.task0101;
+
+/*
+Я думаю, быть программистом круто
+*/
 
 public class Solution {
     public static void main(String[] args) {
         //напишите тут ваш код
-        System.out.println("Амиго очень умный");
+        System.out.println("Я думаю, быть программистом круто");
     }
 }
