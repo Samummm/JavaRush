4f876ca34061148546a56cc6296981a5e695e62d
Mad Max <samumm@mail.ru>
2022-12-29 17:43:54 +0300

    Java Syntax Zero, Level 01, Lecture 03, Task 01

---
+++ Java Syntax Zero/Level 01/Lecture 03/Task 01/Solution.java
@@ -0,0 +1,27 @@
+/**
+ Напиши программу, которая в методе main объявляет такие переменные:
+ name типа String, age типа int и city типа String.
+
+ Примечание: "объявить переменную" - значит то же, что и "создать переменную".
+
+ Требования:
+
+ • Объяви переменную name типа String.
+ • Объяви переменную age типа int.
+ • Объяви переменную city типа String.
+ • Должны быть объявлены 3 переменные.
+ */
+
+package com.javarush.task.task01.task0105;
+
+/*
+Объявляем переменные
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String name, city;
+        int age;
+    }
+}
